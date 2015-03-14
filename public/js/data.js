936723eb34d0ec4d72450b49ea1a6d2ce61937fe
xiaohao@cforth <cforth@163.com>
2015-03-14 20:02:50 +0800

    增加和修改了STOCK对象的API方法

--- public/js/data.js
+++ public/js/data.js
@@ -6,19 +6,40 @@
 //STOCK作为所有全局变量的容器
 var STOCK = {};
 
-//生成JSON格式的数据
-STOCK.data = [
-    {% for stock in stockValues %}
-        { {% for s in stock %}"{{stockKeys[forloop.index0]}}":"{{s}}"{%if forloop.last == false %},{% endif %}{% endfor %} }{%if forloop.last == false %},{% endif %}
-    {% endfor %}];
 
-//根据索引和值来过滤数据
+//使用关联数组保存股票数据，私有属性
+STOCK._data = {
+    {% for stock in stockValues %}
+        "{{stock[1]}}":{ {% for s in stock %}"{{stockKeys[forloop.index0]}}":"{{s}}"{%if forloop.last == false %},{% endif %}{% endfor %} }{%if forloop.last == false %},{% endif %}
+{% endfor %}};
+
+
+//获取STOCK._data的接口方法
+STOCK.getData = function() {
+    return this._data;
+}    
+    
+
+//过滤器方法，根据索引和值来筛选股票数据
 STOCK.filter = function(key, value) {
-    var result = [];
-    for(var i = 0; i < this.data.length; i += 1) {
-        if(this.data[i][key] === value) {
-            result.push(this.data[i]);
+    var stockData = this._data;
+    var result = {};
+    for(var name in stockData) {
+        if(stockData[name][key] === value) {
+            result[name] = stockData[name];
         }
     }
     return result;
+}
+
+
+//查询值方法
+STOCK.getValue = function(id, key) {
+    return STOCK._data[id][key];
+}
+
+
+//更新值方法
+STOCK.setValue = function(id, key, value) {
+    STOCK._data[id][key] = value;
 }
--- public/js/program.js
+++ public/js/program.js
@@ -1,11 +1,32 @@
+// 计算数组的长度
+arrCount = function(o) {
+    var t = typeof o;
+    if(t === 'string'){
+        return o.length;
+    }else if(t === 'object'){
+        var n = 0;
+        for(var i in o){n++;}
+        return n;
+    }
+    return false;
+}
+
+
 //将股票数据插入到网页中
 var insertStockList = function (data) {
+    //将关联数组转换为数组进行排序
+    var arr = [];
+    for(var name in data) {
+        arr.push([name, data[name]]);
+    }
+    arr.sort(function(a,b){return a[1].date < b[1].date ? 1 : -1});
+    
     var text = '<table border = 1>';
     var stock;
     
     text += '<tr><td>股票代码</td><td>所属行业</td><td>关注日期</td><td>初始价</td><td>目标价</td><td>评级</td></tr>';
-    for (var i=0; i<data.length; i++){
-        stock = data[i];
+    for (var i = 0; i < arr.length; i += 1){
+        stock = arr[i][1];
         text = text + '<tr><td>' + stock.id + '</td><td>' + stock.trade + '</td><td>' + stock.date  + '</td><td>' + stock.initPrice + '</td><td>' + stock.targetPrice + '</td><td>' + stock.rate + '</td></tr>';
     }
     text = text +  '</table>';
