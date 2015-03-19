651560c56fad0591ec972ee118f482d22da52ef0
cforth <cforth@163.com>
2015-03-19 14:16:54 +0800

    jsonp测试

--- public/js/data.js
+++ public/js/data.js
@@ -16,17 +16,17 @@ STOCK._hashArr = {
 
 //获取STOCK._hashArr的接口方法
 STOCK.gethashArr = function() {
-    return this._hashArr;
+    return STOCK._hashArr;
 }; 
     
 
 //过滤器方法，根据键值对来筛选数据
 STOCK.filter = function(key, value) {
-    var stockData = this._hashArr;
+    var stockData = STOCK._hashArr;
     var result = {};
-    for(var name in stockData) {
-        if(stockData[name][key] === value) {
-            result[name] = stockData[name];
+    for(var stock in stockData) {
+        if(stockData[stock][key] === value) {
+            result[stock] = stockData[stock];
         }
     }
     return result;
@@ -35,11 +35,55 @@ STOCK.filter = function(key, value) {
 
 //查询值方法
 STOCK.getValue = function(id, key) {
-    return this._hashArr[id][key];
+    return STOCK._hashArr[id][key];
 };
 
 
 //更新或插入键值对的方法
-STOCK.setValue = function(id, key, value) {
-    this._hashArr[id][key] = value;
+STOCK.setValue = function(data) {
+    for(var stock in data) {
+        var s = stock.slice(1);
+        var price = parseFloat(data[stock]['price']);
+        var initPrice = parseFloat(STOCK._hashArr[s]['initPrice']);
+        STOCK._hashArr[s]['name'] = data[stock]['name'];
+        STOCK._hashArr[s]['percent'] = (data[stock]['percent'] * 100).toFixed(2) ;
+        STOCK._hashArr[s]['price'] = data[stock]['price'].toFixed(2);
+        STOCK._hashArr[s]['totalPercent'] = ((price - initPrice) * 100 / initPrice).toFixed(2);
+    }
+};
+
+
+//jsonp获取126股票数据
+STOCK.do_jsonp_id = 1;
+
+STOCK.doJsonp = function(url, callback, timeout) {
+    var name = "do_jsonp_callback_"+(STOCK.do_jsonp_id++);
+    var ele = document.createElement('script');
+    var timer;
+
+    window[name] = function() {
+        //清理计时器
+        window.clearTimeout(timer);
+        //回调回调函数
+        callback.apply(this, arguments);
+        //清理元素
+        ele.parentNode.removeChild(ele);
+        //清理回调
+        delete window[name];
+    };
+
+    url += ((url.indexOf("?") == -1) ? '?' : '&') + "callback=" + name;
+
+    ele.src = url;
+
+    //超时处理
+    timer = window.setTimeout(function(){
+        //清理元素
+        ele.parentNode.removeChild(ele);
+        //清理回调
+        delete window[name];
+    }, timeout||30000);
+
+    //执行请求
+    document.getElementsByTagName('head')[0].appendChild(ele);
 };
--- public/js/program.js
+++ public/js/program.js
@@ -13,8 +13,11 @@ var arrCount = function(o) {
 
 
 //将股票数据插入到网页中
-var insertStockList = function (data) {
+var insertStockList = function (data126) {
+    
+    STOCK.setValue(data126);
     //将关联数组转换为数组进行排序
+    data = STOCK.nowArr;
     var arr = [];
     for(var name in data) {
         arr.push([name, data[name]]);
@@ -24,14 +27,13 @@ var insertStockList = function (data) {
     var text = '<table border = 1>';
     var stock;
     
-    text += '<tr><td>关注日期</td><td>股票代码</td><td>股票名称</td><td>当日涨跌%</td><td>当前价格</td><td>行业分类</td><td>目标价</td><td>累计涨跌%</td></tr>';
+    text += '<tr><td>关注日期</td><td>股票代码</td><td>股票名称</td><td>当日涨跌</td><td>当前价格</td><td>行业分类</td><td>目标价</td><td>累计涨跌</td></tr>';
     for (var i = 0; i < arr.length; i += 1){
         stock = arr[i][1];
         text = text + '<tr><td>' + stock.date + '</td><td>' + stock.id + '</td><td>' + stock.name  + '</td><td>' + stock.percent + '</td><td>' + stock.price + '</td><td>' + stock.trade +'</td><td>' + stock.targetPrice +'</td><td>' + stock.totalPercent +'</td></tr>';
     }
     text = text +  '</table>';
-    
-    return text;
+    document.getElementById("stockShow").innerHTML = text;
 };
 
 
